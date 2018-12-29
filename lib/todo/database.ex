06a41b92da5cb2d6f6573c2ac8e937b8435cd291
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2018-12-28 16:59:45 -0800

    Implement database pooling

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -8,37 +8,37 @@ defmodule Todo.Database do
   end
 
   def store(key, data) do
-    GenServer.cast(__MODULE__, {:store, key, data})
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.store(key, data)
   end
 
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.get(key)
   end
 
+  @impl GenServer
   def init(_) do
     File.mkdir_p!(@db_folder)
-    {:ok, nil}
+    {:ok, start_workers()}
   end
 
-  def handle_cast({:store, key, data}, state) do
-    key
-    |> file_name()
-    |> File.write!(:erlang.term_to_binary(data))
-
-    {:noreply, state}
+  @impl GenServer
+  def handle_call({:choose_worker, key}, _, workers) do
+    pid = workers[:erlang.phash2(key, 3)]
+    {:reply, pid, workers}
   end
 
-  def handle_call({:get, key}, _, state) do
-    data =
-      case File.read(file_name(key)) do
-        {:ok, contents} -> :erlang.binary_to_term(contents)
-        _ -> nil
-      end
-
-    {:reply, data, state}
+  defp choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key})
   end
 
-  def file_name(key) do
-    Path.join(@db_folder, to_string(key))
+  defp start_workers() do
+    for index <- 0..2, into: %{} do
+      {:ok, pid} = Todo.DatabaseWorker.start(@db_folder)
+      {index, pid}
+    end
   end
 end
